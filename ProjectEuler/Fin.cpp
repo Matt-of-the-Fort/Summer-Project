427e5a54533a213373d1ee7ece3afc263b9ba472
micgotschall <micgotschall@Mics-MacBook-Air.local>
2016-12-19 13:40:36 -0800

    testing
    testing123

---
+++ ProjectEuler/Fin.cpp
@@ -0,0 +1,25 @@
+/*#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int sum = 0;
+    int prevNum = 1;
+    int fibNum = 1;
+    
+    while( fibNum < 4000000 ){
+        cout << fibNum << endl;
+        if( fibNum % 2 == 0){
+            sum += fibNum;
+        }
+        int temp = fibNum;
+        fibNum += prevNum;
+        prevNum = temp;
+    }
+    
+    
+    
+    cout << sum;
+    return 0;
+}*/
---
+++ ProjectEuler/palindrone.cpp
@@ -0,0 +1,47 @@
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+int findMax( vector<int>);
+bool palindrone(int n);
+int main(){
+    int mult = 0;
+    vector<int> palinList;
+    
+    for (int num1 = 999; num1 >= 100; num1--){
+        for(int num2 = 999; num2 >= 100; num2--){
+            mult = num1*num2;
+            if( palindrone(mult)){
+                palinList.push_back(mult);
+            }
+        }
+    }
+    
+    cout << findMax( palinList);
+    
+    return 0;
+}
+
+bool palindrone( int n){
+    int rev = 0;
+    int org = n;
+    while( n > 0){
+        rev = (10 * rev) + n % 10;
+        n /= 10;
+    }
+    
+    if( org == rev ){
+        return true;
+    }
+    return false;
+}
+
+int findMax( vector<int> n){
+    int max = 0;
+    for( vector<int>:: const_iterator i = n.begin(); i != n.end(); i++){
+        if( max < *i){
+            max = *i;
+        }
+    }
+    return max;
+}*/
